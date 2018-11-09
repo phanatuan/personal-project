75ba2894144f00c1a8c28efd1690b5483256fc97
Phan Anh Tuan <phananhtuan1011@gmail.com>
2018-11-09 18:02:41 +0700

    My First Push

---
+++ script.js
@@ -0,0 +1,33 @@
+var radios = document.getElementsByName('currency');
+
+function getCurrency() {
+	for (let i=0; i<radios.length; i++) { 
+		if (radios[i].checked) {
+			return(radios[i].value);
+		}
+	}
+}
+
+
+function convert() {
+	var rate = 0;
+	switch (getCurrency()) { 
+		case 'usd':
+			rate = 23390;
+			break;
+		case 'eur':
+			rate = 27000;
+			break;
+		case'bht':
+			rate = 0.8;
+		default:
+			alert('We do not support this currency');
+	}
+
+	var amountInput = parseInt(document.getElementById('amount').value);
+	var amountOutput = amountInput * rate; 
+	var message = `Your ${amountInput}${getCurrency()} is worth ${amountOutput} in VND `;
+	//Output: 
+	document.getElementById('output').innerHTML = message;
+	
+}
